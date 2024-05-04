93a28add358de7509eeae304e121fde442e25161
ZaharPavlikov <94977317+ZaharPavlikov@users.noreply.github.com>
2024-05-04 15:21:38 +0300

    main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_item(name: Optional[str] = Recruto, message: Optional[str] = Давай дружить):
+def read_item(name: Optional[str] = "Recruto", message: Optional[str] = "Давай дружить"):
     data = f"Hello {name}! {message}!"
     return Response(content=data, media_type="text/plain")
