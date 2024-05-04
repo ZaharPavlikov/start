1d72ba2849d69c7839fdfee05ba024b18234aa94
ZaharPavlikov <94977317+ZaharPavlikov@users.noreply.github.com>
2024-05-04 14:10:59 +0300

    main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(name: str, message: str):
+    data = f"Hello {name}! {message}!"
+    return Response(content=data, media_type="text/plain")
+    return {"name": name, "mes": message}
