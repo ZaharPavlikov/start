b2f068dded56688c44c245df5b8c3453437840a6
ZaharPavlikov <94977317+ZaharPavlikov@users.noreply.github.com>
2024-05-04 15:00:40 +0300

    main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_item(name: str=ytyty, message: str=wertyu):
+def read_item(name: Optional[str], message: Optional[str]):
     data = f"Hello {name}! {message}!"
     return Response(content=data, media_type="text/plain")
